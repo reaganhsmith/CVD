74cb863f5f16edd2dd0f5750d720c327a5b73dad
reaganhsmith <reagansmith@byui.edu>
2023-10-10 14:36:55 -0600

    adding style

---
+++ public/js/script.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menuIcon");
+    const closeIcon = document.querySelector(".closeIcon");
+    const menu = document.querySelector(".menu");
+
+    function updateMenuDisplay() {
+        if (window.innerWidth >= 650) {
+            menu.style.display = "inline";
+            menuIcon.style.display = "none";
+            closeIcon.style.display = "block";
+        } else {
+            menu.style.display = "none";
+            closeIcon.style.display = "none";
+            menuIcon.style.display = "block";
+        }
+    }
+
+
+    updateMenuDisplay();
+
+    menuIcon.addEventListener("click", function () {
+        menu.style.display = "inline";
+        menuIcon.style.display = "none";
+        closeIcon.style.display = "block";
+    });
+
+    closeIcon.addEventListener("click", function () {
+        menu.style.display = "none";
+        closeIcon.style.display = "none";
+        menuIcon.style.display = "block";
+    });
+
+    window.addEventListener("resize", updateMenuDisplay);
+});
+
